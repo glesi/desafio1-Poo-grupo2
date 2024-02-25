4740e7220dcefe2243922b644ac1e469fc6b2f76
glesi <glesi.mayandy@gmail.com>
2024-02-25 07:23:42 -0600

    Se agrega archivo gitignore para omitir archivo no utiles en el repositorio remoto, se modifica nombre de paquete para que sea mas generico

--- src/com/desafio/SistemaEstudiantesUdb.java
+++ src/com/desafio/SistemaEstudiantesUdb.java
@@ -4,7 +4,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sistemaestudiantesudb;
+package com.desafio;
 
 import java.util.HashMap;
 import java.util.Map;
